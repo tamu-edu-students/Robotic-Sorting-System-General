dd6407e980de737feb3c38faa4ac5cd648a8b4bb
PaceDominy <113716921+PaceDominy@users.noreply.github.com>
2023-04-21 15:13:13 -0500

    Add files via upload

---
+++ ProjectThreading.py
@@ -0,0 +1,10 @@
+from threading import Thread, enumerate
+import Motor_Logic.py, detect.py, timer_clr_sz_snr.py, spi_12_mean.py, timer_ing_cap.py
+# import utility.py # uncomment if needed
+
+Thread(target=timer_ing_cap.py).start() # takes image for color and size code
+Thread(target=timer_clr_sz_snr.py).start() # color and size code
+Thread(target=spi_12_mean.py).start() # run weight sensor code
+Thread(target=detect.py).start() # run object detection code in a thread.  Uses utility.py
+Thread(target=Motor_Logic.py).start() # run motor logic code in a thread
+enumerate() # check to make sure all the threads are running
