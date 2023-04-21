e8dfb780b6376394229086f76b79a6b14312a9af
PaceDominy <113716921+PaceDominy@users.noreply.github.com>
2023-04-21 15:19:51 -0500

    Add files via upload

--- ProjectThreading.py
+++ ProjectThreading.py
@@ -1,8 +1,9 @@
 from threading import Thread, enumerate
-import Motor_Logic.py, detect.py, timer_clr_sz_snr.py, spi_12_mean.py, timer_ing_cap.py
+import Motor_Logic.py, detect.py, timer_clr_sz_snr.py, spi_12_mean.py, timer_img_cap.py
 # import utility.py # uncomment if needed
+# import lemon_defect_v5.tflite # uncomment if needed
 
-Thread(target=timer_ing_cap.py).start() # takes image for color and size code
+Thread(target=timer_img_cap.py).start() # takes image for color and size code
 Thread(target=timer_clr_sz_snr.py).start() # color and size code
 Thread(target=spi_12_mean.py).start() # run weight sensor code
 Thread(target=detect.py).start() # run object detection code in a thread.  Uses utility.py
